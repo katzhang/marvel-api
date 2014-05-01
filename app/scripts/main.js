c4a37bc3989af36ff597ba4c41744fca0d3c59f2
katzhang <arcadychang@gmail.com>
2014-04-30 23:31:10 -0400

    playing with api data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,18 @@
 console.log(jQuery);
 
+var key = "apikey=587705ba5ff4d0cabadddbbbe5cb3545";
+var entity = "characters?";
+var limit = "limit=100";
+var baseUrl = "http://gateway.marvel.com:80/v1/public/";
+
+var filter = "nameStartsWith=l"
+
+var url = baseUrl + entity + limit + "&" + filter + "&" + key;
+
 $.ajax({
-	url: "http://gateway.marvel.com:80/v1/public/characters?apikey=587705ba5ff4d0cabadddbbbe5cb3545",
-	type: "GET"
+	url: url,
+	type: "GET",
+	success: function(data) {
+		console.log(data);
+	}
 })
