1140dd2f9273e93585ba6968e48d3ccddf5eaa22
katzhang <arcadychang@gmail.com>
2014-05-11 01:31:51 -0400

    added other filters requests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,59 +5,75 @@ var entity = "characters?";
 var limit = "limit=100";
 var baseUrl = "http://gateway.marvel.com:80/v1/public/";
 
-var filter = "nameStartsWith=l"
-
-var url = baseUrl + entity + limit + "&" + filter + "&" + key;
+var url = baseUrl + entity + limit  + "&" + key;
 
 var characters = [];
 
-$.ajax({
-	url: url,
-	cache: true,
-	dataFilter: function(data, type) {
-		if(type === 'json') {
-			var parsed = JSON.parse(data);
-			var chars = parsed.data.results;
-			chars = $.grep(chars, function(char, i) {
-				if(char.thumbnail.path.match("available")) {
-					return false;
-				}
-				return true;
+function ajaxCall(filterLetter) {
+
+	var filter = "nameStartsWith=" + filterLetter;
+	var filteredUrl = url + "&" + filter;
+
+	console.log(filteredUrl);
+
+	return $.ajax({
+		url: filteredUrl,
+		cache: true,
+		dataFilter: function(data, type) {
+			if(type === 'json') {
+				var parsed = JSON.parse(data);
+				var chars = parsed.data.results;
+				chars = $.grep(chars, function(char, i) {
+					if(char.thumbnail.path.match("available")) {
+						return false;
+					}
+					return true;
+				})
+				parsed.data.results = chars;
+			}
+
+			return JSON.stringify(parsed);
+		},
+		dataType: "json",
+		type: "GET",
+		success: function(data) {
+			var chars = data.data.results;
+			console.log(data);
+			console.log(chars);
+
+			chars.forEach(function(character) {
+				var name = character.name;
+				var des = character.description;
+				var thumbnailPath = character.thumbnail.path;
+				var thumbnailExt = character.thumbnail.extension;
+				var storiesNum = character.stories.available;
+
+				thumbnailPath += "/standard_xlarge" + "." + thumbnailExt;
+
+				characters.push({"charName": name, "value": storiesNum, "imgPath": thumbnailPath});
+
 			})
-			parsed.data.results = chars;
 		}
+	})
+}
 
-		return JSON.stringify(parsed);
-	},
-	dataType: "json",
-	type: "GET",
-	success: function(data) {
-		var chars = data.data.results;
-		console.log(chars);
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+var ajaxCalls = [];
 
-		chars.forEach(function(character) {
-			var name = character.name;
-			var des = character.description;
-			var thumbnailPath = character.thumbnail.path;
-			var thumbnailExt = character.thumbnail.extension;
-			var storiesNum = character.stories.available;
+for(var i = 0; i < alphabet.length; i++) {
+	ajaxCalls.push(ajaxCall(alphabet[i]));
+	console.log(ajaxCalls);
+}
 
-			thumbnailPath += "/standard_xlarge" + "." + thumbnailExt;
-
-			characters.push({"charName": name, "value": storiesNum, "imgPath": thumbnailPath});
-
-			// $('.marketing').append('<h6>' + name + '</h6>');
-			// $('.marketing').append('<img src="' + thumbnailPath + '"/>')
-
-		})
-
-		characters = {name: "marvel", children: characters};
-		// characters = {"children": characters};
-
-		console.log(characters);
-		bindData(characters);
-	}
+$.when.apply(this, ajaxCalls).done(function() {
+	console.log(characters);
+	bindData({name: 'marvel', children: characters});
 })
+
+// $.when(ajaxCall('a')).done(function() {
+// 	console.log(characters);
+// 	bindData({name: 'marvel', children: characters});
+// })
 
 var diameter = 960;
 var format = d3.format(",d");
@@ -96,10 +112,10 @@ function bindData(charsData) {
 
     addPatterns
 
-  	node.append("text")
-      	.attr("dy", ".3em")
-      	.style("text-anchor", "middle")
-      	.text(function(d) { return d.name.substring(0, d.r / 3); });
+  	// node.append("text")
+   //    	.attr("dy", ".3em")
+   //    	.style("text-anchor", "middle")
+   //    	.text(function(d) { return d.charName.substring(0, d.r / 3); });
 }
 
 function addPatterns(character) {
