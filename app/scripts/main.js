4a6030758b4f4be371f9e74bbd3e6cf5fdd5e7ab
katzhang <arcadychang@gmail.com>
2014-05-29 22:03:59 -0400

    finished cleaning up code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,6 +23,7 @@ var Ma = {
 	},
 
 	ajaxCalls: [],
+	ajaxSeriesCalls: [],
 
 	apiCall: function(url, callBack, dataFilter) {
 		var settings = {
@@ -149,10 +150,240 @@ var Ma = {
 		$.when.apply(Ma, Ma.ajaxCalls).done(function() {
 			console.log(Ma.data.characters);
 			Ma.bindBubbleData({name: 'marvel', children: Ma.data.characters});
+			Ma.seriesEventHandler();
+		})
+
+	},
+
+	callSeries: function(url, total, i, charId) {
+		return $.ajax({
+				url: url + '&offset=' + i*100,
+				type: "GET",
+				success: function(data) {
+					console.log(data)
+					console.log('total: ' + total);
+					Ma.parseSeriesDataHelper(data, charId);
+				}
+			})
+	},
+
+	parseSeriesDataHelper: function(data, charId) {
+		var series = data.data.results;
+
+		series.forEach(function(serie) {
+			var title = serie.title;
+			var id = serie.id;
+			var startYear = serie.startYear;
+
+			if(startYear > 0) {
+				Ma.data.series[charId].push({seriesTitle: title, seriesId: id, seriesStartYear: startYear});
+			}
+
+		})
+
+		console.log(Ma.data.series);
+	},
+
+	parseSeriesData: function(charId) {
+		var lineData = Ma.countProp(Ma.data.series[charId]);
+		lineData.sort(function(a,b) {
+			return a.date - b.date;
+		})
+		return lineData;
+	},
+
+	countProp: function(array) {
+		var output = [];
+
+		for(var i = 0; i < array.length; i++) {
+			var obj = array[i];
+
+			var needNew = output.every(function(el, index) {
+							return el['date'] != obj.seriesStartYear;
+						});
+
+			if(needNew) {
+				output.push({date: obj.seriesStartYear.toString(), close: 1});
+			} else {
+				output.forEach(function(el, index) {
+					if(el.date == obj.seriesStartYear) {
+						el.close += 1;
+					}
+				})
+			}
+			
+
+		}
+
+		return output;
+	},
+
+	bindLineChart: function(data, context) {
+		var margin = {top: 20, right: 20, bottom: 30, left: 50},
+		    // width = 960 - margin.left - margin.right,
+		    // height = 500 - margin.top - margin.bottom;
+
+			width = $('.modal-content').width() - margin.left - margin.right, height = 400;
+
+		var parseDate = d3.time.format("%Y").parse;
+
+		// var parseDate = d3.time.format("%d-%b-%y").parse;
+
+		var x = d3.time.scale()
+		    .range([0, width]);
+
+		var y = d3.scale.linear()
+		    .range([height, 0]);
+
+		var xAxis = d3.svg.axis()
+		    .scale(x)
+		    .orient("bottom");
+
+		var yAxis = d3.svg.axis()
+		    .scale(y)
+		    .orient("left");
+
+		var line = d3.svg.line()
+		    .x(function(d) { return x(d.date); })
+		    .y(function(d) { return y(d.close); });
+
+		var svg = d3.select(context).append("svg")
+		    .attr("width", width + margin.left + margin.right)
+		    .attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		data.forEach(function(d) {
+			d.date = parseDate(d.date);
+			d.close = +d.close;
+
+		})
+
+
+
+		  x.domain(d3.extent(data, function(d) { console.log(d); return d.date; }));
+		  y.domain(d3.extent(data, function(d) { return d.close; }));
+
+		  console.log(width);
+		  console.log(height);
+
+		  svg.append("g")
+		      .attr("class", "x axis")
+		      .attr("transform", "translate(0," + height + ")")
+		      .call(xAxis);
+
+		  svg.append("g")
+		      .attr("class", "y axis")
+		      .call(yAxis)
+		    .append("text")
+		      .attr("transform", "rotate(-90)")
+		      .attr("y", 6)
+		      .attr("dy", ".71em")
+		      .style("text-anchor", "end")
+		      .text("Price ($)");
+
+		  svg.append("path")
+		      .datum(data)
+		      .attr("class", "line")
+		      .attr("d", line);
+
+	},
+
+	seriesEventHandler: function() {
+		$('svg').on('click', 'g', function(e) {
+			var charId = $(this).data('char-id');
+			var charData = Ma.data.characters;
+			var character;
+
+			console.log(charId);
+			$('.modal-content').html(charId);
+
+
+			for(var i = 0; i < charData.length; i++) {
+				if(charData[i].id == charId) {
+					character = charData[i];
+				}
+			}
+
+			var eventsUrl = character.seriescollectionURI;
+			var url = eventsUrl + "?" + key + '&limit=100';
+			var total;
+
+			$.ajax({
+				url: url,
+				dataType: "json",
+				type: "GET",
+				success: function(data, charId) {
+					console.log(data)
+					total = data.data.total;
+					Ma.data.series[charId] = [];
+					console.log(total);
+					Ma.parseSeriesDataHelper(data, charId);
+
+					for(var i = 1; i < total/100; i++) {
+						Ma.ajaxSeriesCalls.push(Ma.callSeries(url, total, i, charId));
+					}
+
+					$.when.apply(this, Ma.ajaxSeriesCalls).done(function() {
+						var lineData = Ma.parseSeriesData(charId);
+						Ma.bindLineChart(lineData, '.modal-content');
+					})
+
+					// callSeries(url, total);
+				}
+			})
+
+
 		})
 	}
 
 }
+
+// $('svg').on('click', 'g', function(e) {
+// 	var charId = $(this).data('char-id');
+// 	var charData = Ma.data.characters;
+// 	var character;
+
+// 	console.log(charId);
+// 	$('.modal-content').html(charId);
+
+
+// 	for(var i = 0; i < charData.length; i++) {
+// 		if(charData[i].id == charId) {
+// 			character = charData[i];
+// 		}
+// 	}
+
+// 	var eventsUrl = character.seriescollectionURI;
+// 	var url = eventsUrl + "?" + key + '&limit=100';
+// 	var total;
+
+// 	$.ajax({
+// 		url: url,
+// 		dataType: "json",
+// 		type: "GET",
+// 		success: function(data, charId) {
+// 			console.log(data)
+// 			total = data.data.total;
+// 			Ma.dataseries[charId] = [];
+// 			console.log(total);
+// 			Ma.parseSeriesDataHelper(data, charId);
+
+// 			for(var i = 1; i < total/100; i++) {
+// 				Ma.ajaxSeriesCalls.push(Ma.callSeries(url, total, i, charId));
+// 			}
+
+// 			$.when.apply(this, Ma.ajaxSeriesCalls).done(function() {
+// 				var lineData = Ma.parseSeries(charId);
+// 				lMa.ineChart(lineData, '.modal-content');
+// 			})
+
+// 			// callSeries(url, total);
+// 		}
+// 	})
+
+
+// })
 
 var key = "apikey=587705ba5ff4d0cabadddbbbe5cb3545";
 var entity = "characters?";
